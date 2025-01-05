cd12eb595c073bbc33b31df8fd69aaee989124da
robert-winton <rob.win.93@gmail.com>
2025-01-05 19:51:51 +0000

    Update streamlit_app.py
    loc and iloc functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit
 
 #Convert from SNowpark df to Pandas df so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 
 ingredients_list = st.multiselect(
@@ -38,11 +38,15 @@ if ingredients_list:
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
         ingredients_string += fruit_chosen + ' '
-    
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+        
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
