64aa92a66f95a8aed4be484428ba83a469a50145
robert-winton <rob.win.93@gmail.com>
2025-01-05 19:48:59 +0000

    Update streamlit_app.py
    #Convert from SNowpark df to Pandas df so we can use the LOC function

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -17,7 +18,12 @@ st.write("The name on your smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width = True)
+# st.dataframe(data=my_dataframe, use_container_width = True)
+# st.stop()
+
+#Convert from SNowpark df to Pandas df so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 
