f021b76296e7fe81bac69a3a02a7268e712bd924
robert-winton <rob.win.93@gmail.com>
2025-01-05 19:45:12 +0000

    Update streamlit_app.py
    reverted lines, added checks for dataframe and SEARCH_ON col

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,14 +16,15 @@ st.write("The name on your smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'),col('SEARCH_ON'))
+st.dataframe = (data=my_dataframe, use_container_width = True)
+st.stop()
 
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    my_dataframe
-    #my_dataframe, 
-    #max_selections = 5
+    my_dataframe, 
+    max_selections = 5
 )
 
 if ingredients_list:
